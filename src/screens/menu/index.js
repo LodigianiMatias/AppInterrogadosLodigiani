fbddd9106cd408c7f39837fce3c462563db0c77b
Matias <lodigiani.matias97@gmail.com>
2022-10-24 22:19:38 -0300

    Building expo

--- src/screens/menu/index.js
+++ src/screens/menu/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { View, Text, Button, FlatList, TouchableOpacity } from "react-native";
+import { View, Text, Button, FlatList, TouchableOpacity, ScrollView } from "react-native";
 import { colors } from "../../constants/themes";
 import {styles} from "./styles";
 
 const MainMenu = ({navigation}) => {
     
     return (
-        <View style={styles.container}>
+        <ScrollView>
+            <View style={styles.container}>
             <Text style={styles.title}>Interrogados</Text>
             <Text style={styles.text}>El juego consiste en responder preguntas al estilo trivia, ganarás si logras 3 respuestas correctas. Si respondes incorrectamente se reiniciará la partida</Text>
             <Button
@@ -16,6 +17,7 @@ const MainMenu = ({navigation}) => {
             onPress={() => navigation.navigate("Categories")}
             >Comenzar Juego</Button>
         </View>
+        </ScrollView>
     )
 }
 
