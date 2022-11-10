14318d79e34585e8131765e396501ae16dc0e927
Matias <-lodigiani.matias97@gmail.com>
2022-11-10 19:20:25 -0300

    Updating

--- src/screens/credits/index.js
+++ src/screens/credits/index.js
@@ -1,13 +1,22 @@
 import { Text, View } from "react-native";
 
+import { LocationSelector } from "../../components";
 import React from "react";
 import { styles } from "./styles";
+import { useState } from "react";
 
 const Credits = ({navigation}) => {
 
+    const [location, setLocation] = useState("")
+    const onHandlerLocation = (location) => {
+        setLocation(location);
+    }
+
+    
     return (
         <View style={styles.container}>
             <Text style={styles.title}>¡Gracias por jugar mi juego!</Text>
+            <LocationSelector onLocation={onHandlerLocation}/>
         </View>
     )
 }
