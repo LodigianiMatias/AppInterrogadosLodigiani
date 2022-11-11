47cf5e77267ea72e44b0eca65e6c09b34058e9f3
Matias <lodigiani.matias97@gmail.com>
2022-11-10 21:05:00 -0300

    Updates

--- src/components/question-item/index.js
+++ src/components/question-item/index.js
@@ -1,4 +1,4 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Button, Image, Text, TouchableOpacity, View } from "react-native";
 
 import React from "react";
 import { styles } from "./styles";
--- src/screens/menu/index.js
+++ src/screens/menu/index.js
@@ -4,12 +4,11 @@ import React from "react";
 import {styles} from "./styles";
 
 const MainMenu = ({navigation}) => {
-    
     return (
         <ScrollView style={styles.scroll}>
             <View style={styles.container}>
             <Text style={styles.title}>Interrogados</Text>
-            <Text style={styles.text}>El juego consiste en responder preguntas al estilo trivia, ganarás si logras 3 respuestas correctas. Si respondes incorrectamente se reiniciará la partida</Text>
+            <Text style={styles.text}>El juego consiste en responder preguntas al estilo de la clásica trivia, obtendrás la victoria si logras 3 respuestas correctas. Si respondes incorrectamente se reiniciará la partida. ¡Buena suerte!</Text>
             <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate("Categories")}>
                 <Text style={styles.buttonText}>¡Comenzar a jugar!</Text>
             </TouchableOpacity>
